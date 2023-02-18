ce7b646c0299c94216d52dc8ee710808375da3eb
AI-Amponsah <yawmap27@gmail.com>
2023-02-18 11:59:05 +0000

    temp

---
+++ Tutorials/Temperature_conversion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/***
+ * Write a program that allows the user to enter a floating-point number 
+ *representing degrees Celsius, and then displays the corresponding degrees 
+ *Fahrenheit. 
+*/
+
+int main(void)
+{
+    float tempearture;
+
+    cout << "Enter a temperature in degree Celsius: ";
+    cin >> tempearture;
+
+    tempearture = (tempearture * 9/5) + 32;
+    cout << "Your Equivalent Temperature in Fahrenheit is: " << tempearture << endl;
+    return (0);
+}
