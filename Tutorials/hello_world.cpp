e80252ffd791e4b796789f0da2bf6433187b5152
AI-Amponsah <yawmap27@gmail.com>
2023-02-18 11:32:05 +0000

    messing

--- Tutorials/hello_world.cpp
+++ Tutorials/hello_world.cpp
@@ -3,13 +3,23 @@ using namespace std;
 
 int main(void)
 {
+    int age, a ,b;
+    int sum = 0;
     cout << "Hello World!" << endl;
     char c = 'A';
     cout << c << endl ;
 
-    int age;
+    //int age;
     cout << "Enter your age: " ;
     cin >> age;
     cout <<"Your age is " << age <<"." << endl;
+
+    cout << "Enter first number: "<< endl;
+    cin >> a;
+    cout << "Enter seconf number: "<< endl;
+    cin >> b;
+
+    sum =  a + b;
+    cout << "The sum is: " << sum << endl;
     return (0);
 }
