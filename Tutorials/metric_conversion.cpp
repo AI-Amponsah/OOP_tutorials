690c1456cfe6a30d1fd53d42ca2dca016c5651f1
AI-Amponsah <yawmap27@gmail.com>
2023-02-18 11:45:50 +0000

    mess

---
+++ Tutorials/metric_conversion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Assuming there are 7.481 gallons in a cubic foot, write a program that asks 
+ *the user to enter a number of gallons, and then displays the equivalent in 
+ *cubic feet
+*/
+
+int main(void)
+{
+    return (0);
+}
