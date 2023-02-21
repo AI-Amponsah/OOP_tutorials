5aab1afc6ff82234f17721d0e203f9e8200ffef7
AI-Amponsah <yawamp27@gmail.com>
2023-02-21 04:06:38 +0000

    private

--- Class_Objects/class.cpp
+++ Class_Objects/class.cpp
@@ -6,27 +6,43 @@ using namespace std;
  * An object comprises of a data part and  a member function
 */
 
-class smallOBj
+class small
 {
     private:
      int data;
     public:
         string name;
         int age;
+	
+	// using set_data to change the value of a private member data
+	void set_data(int change_data)
+	{
+		data = change_data;
+
+	}
+	//using get_data to acess the or retun the value of a private data member
+	int  get_data()
+	{
+		return (data);
+	}
         void print()
         {
-            cout  << age << endl;
+            cout  << name << endl;
 
         }
 };
 
 int main(void)
 {
-    smallOBj person;
+    small person;
     person.name = "Isaac";
     person.age = 98;
+    //person.data = 1000;  // Trying to acess a private member 
 
     person.print();
+    person.set_data(1000);
+
+    cout << person.get_data() << endl;
 
     return (0);
 }
