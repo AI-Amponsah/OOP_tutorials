def8ff366635e87cebb7149f2cece4c0a22ccfb9
AI-Amponsah <yawmamp27@gmail.com>
2023-02-21 02:47:28 +0000

    classes

---
+++ Class_Objects/class.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * classes act as blueprints used to instantiate objects
+ * An object comprises of a data part and  a member function
+*/
+
+class smallOBj
+{
+    private:
+     int data;
+    public:
+        int some_function()
+        {
+
+        }
+        void show_data(int d)
+        {
+
+        }
+
+};
+
+int main(void)
+{
+
+    return (0);
+}
