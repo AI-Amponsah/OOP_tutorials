34abbe9233c4427fe5fa8c83a368a804137e2fb8
AI-Amponsah <yawmap27@gmail.com>
2023-02-21 02:18:23 +0000

    OOP

---
+++ Tutorials/Temp_conversion.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Write a temperature-conversion program that gives the user the option of converting 
+ * Fahrenheit to Celsius or Celsius to Fahrenheit. Then carry out the conversion.
+*/
+
+int main(void)
+{
+    int temp;
+    float Fahrenheit;
+    float Celsius;
+    char choice;
+
+    cout << " Enter F to convert to Fahrenheit and C to Celsius: " ;
+    cin >> choice;
+
+    if (choice == 'F')
+    {
+        cout << "Enter the temperature in Fahrenheit: ";
+        cin >> Fahrenheit;
+
+        Celsius  =  (Fahrenheit - 32 ) * 5 / 9;
+
+        cout << "Equivalent Temperature in Celsius is: " << Celsius << endl;
+    }
+    else if (choice == 'C')
+    {
+        cout << "Enter the temperature in Celsius: ";
+        cin >> Celsius;
+
+       Fahrenheit =  (Celsius * 9/5 ) + 32;
+
+        cout << "Equivalent Temperature in Fahrenheit is: " << Fahrenheit << endl;
+    }
+
+    return (0);
+}
