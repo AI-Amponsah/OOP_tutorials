f1c56583431339e96f1d289d95a9de7a73db0c20
AI-Amponsah <yawmamp27@gmail.com>
2023-03-12 12:07:11 +0000

    hmm

--- DVLA/owner.cpp
+++ DVLA/owner.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
-
+void Owner_inputs();
 class Owner
 {
     private:
@@ -13,9 +14,16 @@ class Owner
 
             void print()
             {
+                ofstream outfile("json.txt",ios::out | ios_base::app);
+                outfile << name << " " << address << " " << phone_number << endl;
+                outfile.close();
+                
+
+                /**
                  cout << "Name of current owner is: " <<  name << endl;
                  cout << "Address of current owner: " << address << endl;
-		 cout << "Telephone number of owner: " << phone_number << endl;
+		        cout << "Telephone number of owner: " << phone_number << endl;
+                */
             }
         };
     public:
@@ -33,8 +41,17 @@ class Owner
 
 int main(void)
 {
+    Owner_inputs();
+    
+    return (0);
+}
+
+void Owner_inputs()
+{
+    do{
     Owner owner1;
     string name, address, number;
+    
     cout << "Enter name: ";
     cin >> name;
     cout << "Enter address: ";
@@ -42,7 +59,8 @@ int main(void)
     cout << "Enter Phone Number: ";
     cin >> number;
     cout << endl;
-    
     owner1.set_owner(name, address, number);
-    return (0);
+    }while(1);
+    
+
 }
