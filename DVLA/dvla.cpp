3230a810f16cd905178f34682dc638983ac04427
AI-Amponsah <yawmamp27@gmail.com>
2023-03-13 22:38:31 +0000

    seed

--- DVLA/dvla.cpp
+++ DVLA/dvla.cpp
@@ -1,82 +1,59 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 class Owner {
     private:
-        struct Item {
-            string name;
-            string address;
-        } item_;
+        string name_;
+        string address_;
     public:
-        Owner(string name, string addr) {
-            item_.name = name;
-            item_.address = addr;
-        }
-        
+        Owner(string name, string address) : name_(name), address_(address) {}
+
         void print() {
-            cout << "Name: " << item_.name << endl;
-            cout << "Address: " << item_.address << endl;
-        }  
+            cout << "Name: " << name_ << endl;
+            cout << "Address: " << address_ << endl;
+        }
 };
 
-class Vehicle {
+class Vehicle : public Owner {
     private:
         string brand_;
         string model_;
         string num_plate_;
-        Owner owner_;
     public:
-        Vehicle(string brand, string model, string num_plate, Owner owner)
-            : brand_(brand), model_(model), num_plate_(num_plate), owner_(owner) {}
+        Vehicle(string brand, string model, string num_plate, string name, string address)
+            : Owner(name, address), brand_(brand), model_(model), num_plate_(num_plate) {}
 
         void set_details()
         {
-            string name, address;
-            cout << "Enter owner's name: ";
-            cin >> name;
-            cout << "Enter owner's address: ";
-            cin >> address;
-            owner_ = Owner(name, address);
-            
-            cout << "Enter brand: ";
-            cin >> brand_;
-            cout << "Enter model: ";
-            cin >> model_;
-            cout << "Enter number plate: ";
-            cin >> num_plate_;
-            cout << endl;
+
+            // generate a random number for the vehicle number plate
+            srand(time(NULL)); 
+            int rand_num = rand() % 10000 + 1000; /
+            num_plate_ = "ABC " + to_string(rand_num); 
+            cout << "Vehicle number plate: " << num_plate_ << endl << endl;
         }
-        
+
         void print()
         {
-            owner_.print();
+            Owner::print();
             cout << "Brand: " << brand_ << endl;
             cout << "Model: " << model_ << endl;
             cout << "Number Plate: " << num_plate_ << endl;
-        }     
+        }
 };
+
 
 int main(void) 
 {
-    string owner_name, owner_address, vehicle_brand, vehicle_model, vehicle_num_plate;
+    string owner_name, owner_address, vehicle_brand, vehicle_model;
+    Vehicle car1(vehicle_brand, vehicle_model, "", owner_name, owner_address);
     
-    cout << "Enter owner's name: ";
-    getline(cin, owner_name);
-    cout << "Enter owner's address: ";
-    getline(cin, owner_address);
-    Owner owner(owner_name, owner_address);
-
-    cout << "Enter vehicle brand: ";
-    getline(cin, vehicle_brand);
-    cout << "Enter vehicle model: ";
-    getline(cin, vehicle_model);
-    cout << "Enter vehicle number plate: ";
-    getline(cin, vehicle_num_plate);
-    Vehicle car1(vehicle_brand, vehicle_model, vehicle_num_plate, owner);
-    
+    car1.set_details();
     car1.print();
     
     return 0;
