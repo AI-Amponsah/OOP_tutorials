eea1621ccb99590ea3363959c3ad910b693a6e8e
AI-Amponsah <yawmamp27@gmail.com>
2023-04-03 14:13:07 +0000

    loop

--- DVLA/main.cpp
+++ DVLA/main.cpp
@@ -3,11 +3,14 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+
+// Include the header files for the Vehicle and Owner classes
 #include "Vehicle.h"
 #include "Owner.h"
 
 using namespace std;
 
+// Function to display the main menu
 void display_menu(void) 
 {
     cout << "Welcome to DVLA database\n";
@@ -19,8 +22,9 @@ void display_menu(void)
     cout << "5. Exit\n";
 }
 
+// Function to generate a random number plate
 string generate_num_plate(void)
- {
+{
     string num_plate = "ABC-";
     srand(time(NULL));
     for (int i = 0; i < 3; i++) {
@@ -29,14 +33,23 @@ string generate_num_plate(void)
     return num_plate;
 }
 
+// Function to enter vehicle and owner details
 void enter_details(vector<Owner>& records) 
 {
+    // Variables to store user input
     string brand, model, year_str, name, address, phone_number;
-    string num_plate = generate_num_plate();
-    time_t now = time(NULL);
-    struct tm* expiration_date = localtime(&now);
-    expiration_date->tm_year += 3; // add 3 years to expiration date
 
+    // Generate a random number plate
+    string num_plate = generate_num_plate();
+
+    // Get the current time
+    time_t now = time(NULL);
+
+    // Add 3 years to the current time to get the expiration date
+    struct tm* expiration_date = localtime(&now);
+    expiration_date->tm_year += 3;
+
+    // Get input from the user
     cout << "Enter Vehicle Brand: ";
     cin.ignore();
     getline(cin, brand);
@@ -45,7 +58,6 @@ void enter_details(vector<Owner>& records)
     cout << "Enter year of Registration: ";
     getline(cin, year_str);
     int year = stoi(year_str);
-
     cout << "Enter owner name: ";
     getline(cin, name);
     cout << "Enter owner address: ";
@@ -53,29 +65,47 @@ void enter_details(vector<Owner>& records)
     cout << "Enter owner phone number: ";
     getline(cin, phone_number);
 
+    // Create a new Owner object with the input data and the generated number plate
     Owner record(num_plate, brand, model, year, name, address, phone_number);
+
+    // Set the expiration date for the new record
     record.set_expiration_date(*expiration_date);
+
+    // Add the new record to the vector of records
     records.push_back(record);
 
+    // Print the number plate and expiration date for the new record
     cout << "Record added. Number plate: " << num_plate << "\n";
     cout << "Expiration date: " << asctime(expiration_date) << "\n";
 }
 
+// Function to renew the license for a vehicle
 void renew_license(vector<Owner>& records)
- {
+{
+    // Variable to store the user's search query
     string search_plate;
     bool found = false;
 
+    // Get the number plate to search for from the user
     cout << "Enter number plate to renew license: ";
     cin >> search_plate;
 
+    // Search through the vector of records for the given number plate
     for (auto& record : records) {
         if (record.get_num_plate() == search_plate) {
             found = true;
+
+            // Get the current time
             time_t now = time(NULL);
+
+            // Add 3 years to the current time to get the new expiration date
             struct tm* expiration_date = localtime(&now);
-            expiration_date->tm_year += 3; // add 3 years to expiration date
+            expiration_date->tm_year += 3;
+
+            // Set the new expiration date for the record
             record.set_expiration_date(*expiration_date);
+
+            // Print the updated expiration
             cout << "License renewed for number plate: " << search_plate << "\n";
             cout << "New expiration date: " << asctime(expiration_date) << "\n";
             break;
@@ -87,44 +117,49 @@ void renew_license(vector<Owner>& records)
     }
 }
 
-
-
-
+// Function to display details of all records in the vector
 void display_details(const vector<Owner>& records) {
+    // Iterate through each record in the vector
     for (const auto& record : records) {
+        // Print the details of the current record
         record.print();
+        // Print a new line
         cout << endl;
     }
 }
-
-void search_details(const vector<Owner>& records) {
-    string search_plate;
-    bool found = false;
-
+/** This function takes a vector of Owner objects as input and searches for 
+a specific record based on the number plate entered by the user.
+*/
+void search_details(const vector<Owner>& records) 
+{
+    string search_plate; // variable to hold the number plate entered by the user
+    bool found = false; // flag variable to indicate whether the record was found or not
+// Prompt the user to enter the number plate to search for
     cout << "Enter number plate to search for: ";
     cin >> search_plate;
-
+// Loop through the records vector to search for the record with the entered number plate
     for (const auto& record : records) {
-        if (record.get_num_plate() == search_plate) {
-            found = true;
+        if (record.get_num_plate() == search_plate) // check if the current record matches the entered number plate
+        {
+            found = true; // set the found flag to true
             cout << "Record found.\n";
-            record.print();
-            break;
+            record.print();  // print the details of the found record
+            break; // exit the loop once the record has been found
         }
     }
-
+// If the record was not found, display a message to the user
     if (!found) 
     {
         cout << "Record not found.\n";
     }
 }
-
+// main / entry point function that runs the program
 int main(void) {
     vector<Owner> records;
-
+// loop until the user chooses to exit the program
     while (true) 
     {
-        display_menu();
+        display_menu(); // display the main menu and get the user's choice
         int choice;
         cin >> choice;
 
--- DVLA/owner.cpp
+++ DVLA/owner.cpp
@@ -1,102 +1,23 @@
-#include <iostream>
-#include <string>
-#include <fstream>
+#include "owner.h"
 using namespace std;
-void Owner_inputs();
-void display();
-class Owner
-{
-    private:
-        struct owner
-        {
-            string name;
-            string address;
-            string phone_number;
 
-            void serialize()
-            {
-                ofstream outfile("json.txt",ios::out | ios_base::app);
-                outfile << name << " " << address << " " << phone_number << endl;
-                outfile.close();
-                
-
-                /**
-                 cout << "Name of current owner is: " <<  name << endl;
-                 cout << "Address of current owner: " << address << endl;
-		        cout << "Telephone number of owner: " << phone_number << endl;
-                */
-            }
-        };
-    public:
-        void set_owner(string name, string address, string number)
-        { 
-            owner s;
-            s.name = name;
-            s.address = address;
-	        s.phone_number = number;
-            s.serialize();     
-        }
-
-        void Owner_inputs()
-        {
-            Owner owner1;
-            string name, address, number;
-    
-            cout << "Enter name: ";
-            cin >> name;
-            cout << "Enter address: ";
-            cin >> address;
-            cout << "Enter Phone Number: ";
-            cin >> number;
-            cout << endl;
-            owner1.set_owner(name, address, number);
-        }
-
-        void display()
-        {
-            ifstream ifs("json.txt");
-            string data;
-            while(ifs.good())
-            {
-                ifs >> data ;
-                if (ifs.good())
-                {
-                    cout  << data.size();
-                    cout << " ";
-                }
-            }
-            cout << endl;
-            ifs.close();
-
-}
-};
-class Vehicle : public Owner 
-{
-    private:
-        string  brand;
-        string model;
-        string number;
-    
-    public:
-        void car_details()
-        {
-            cout << "Enter brand: ";
-            cin >> brand;
-            cout << "Enter model: ";
-            cin >> model;
-            //cout << "Enter Phone Number: ";
-            //cin >> number;
-        }
-   
-};
-
-int main(void)
-{
-    Vehicle person;
-    person.display();
-    person.Owner_inputs();
-    person.car_details();
-    return (0);
+Owner::Owner(string np, string br, string mo, int yr, string n, string addr, string phone) : Vehicle(np, br, mo, yr)
+ {
+    name = n;
+    address = addr;
+    phone_number = phone;
 }
 
+void Owner::set_expiration_date(struct tm date) 
+{
+    expiration_date = date;
+}
 
+void Owner::print() const 
+{
+    Vehicle::print();
+    cout << "Name: " << name << endl;
+    cout << "Address: " << address << endl;
+    cout << "Phone Number: " << phone_number << endl;
+    cout << "Expiration date: " << asctime(&expiration_date) << endl;
+}
